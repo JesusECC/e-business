6632ae6e31f7b00d5227ec8aa40776fa668abe6a
jacrproyect <jcastillo@jacrproyectosweb.com>
2018-05-27 22:54:59 -0500

    Paquete vita

---
+++ resources/views/main/paquete.blade.php
@@ -0,0 +1,47 @@
+@extends ('layouts.pagina')
+@section ('contenido')
+     <div class="content-box">
+     	<div class="wrap">
+     		<ul class="events">
+				<li class="active"><a href="main.index">Eventos Recientes</a></li> 
+				
+			</ul>
+     	   <div class="clear"></div>
+     	</div>
+     </div>
+     <div class="main">
+     	@foreach ($evento as $eve)
+     	<div class="wrap">
+     		<div class="section group">
+     			
+				<div class="col_1_of_3 span_1_of_3">
+					<img src="{{asset('images/eventos/'.$eve->imagen)}}" alt=""/>
+					<ul class="m_fb">
+						<li>
+							<span class="m_22"><a href="#"><img src="{{asset('images/fb.png')}}" alt=""/></a>
+							</span>
+
+						    <span class="m_23"><a href="#"><img src="{{asset('images/heart.png')}}" alt=""/></a></span>
+						     <div class="clear"></div>
+						</li>
+					</ul>
+					  <div class="desc">
+						<h3><a href="#">{{$eve->nombre}}</a></h3>
+						<h4 class="m_2">Fecha :{{$eve->fecha}} - Hora: {{$eve->hora}}</h4>
+						<h5 class="m_3">{{$eve->descripcion}}</p>
+					   </div>
+					   <div class="section group example">
+						<center >
+							<div class="get_btn1"><a href="">Ver paquetes</a></div>
+						</center>
+						
+						<div class="clear"></div>
+		    		  </div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+		@endforeach
+     </div>
+     {{$evento->render()}}
+@endsection
